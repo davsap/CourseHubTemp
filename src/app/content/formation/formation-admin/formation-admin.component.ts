774cd59fb6a8b21675f71152bb1a50ad4656062b
davsap <david.parto@hotmail.com>
2018-09-16 14:00:50 +0200

    admin updated

--- src/app/content/formation/formation-admin/formation-admin.component.ts
+++ src/app/content/formation/formation-admin/formation-admin.component.ts
@@ -12,16 +12,24 @@ import { Formation } from '../../../model/formation';
 export class FormationAdminComponent implements OnInit {
 
 constructor(private service: FormationService, private route: ActivatedRoute, private router: Router) { }
-  formation: Observable<any>;
+  formation: Observable<Formation>;
 // formation: Formation;
 
   ngOnInit() {
     const id = +this.route.snapshot.paramMap.get('id');
-   this.service.getFormationV2(id).subscribe((value: Observable<any>) => this.formation = value);
+   this.service.getFormationV2(id).subscribe((value: Observable<Formation>) => this.formation = value);
     // this.service.getFormationV2(id).subscribe((value: Formation) => this.formation = value);
     }
 
     addSujet() {
       this.router.navigateByUrl('/formationadmin/' + this.route.snapshot.paramMap.get('id') + '/sujet');
+      }
+    addCours(sujetid: any) {
+   //   console.log('sujet id is :' + id);
+     this.router.navigateByUrl('/formationadmin/' + this.route.snapshot.paramMap.get('id') + '/sujet/' + sujetid + '/cours');
+      }
+    editCours(sujetid: any, coursid: any) {
+   //   console.log('sujet id is :' + id);
+     this.router.navigateByUrl('/formationadmin/' + this.route.snapshot.paramMap.get('id') + '/sujet/' + sujetid + '/cours/' + coursid);
       }
 }
